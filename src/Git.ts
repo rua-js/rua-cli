0217e67b4ddacdc3f45746e368264a4e6bbce0ad
copydog <775150@qq.com>
2018-01-26 00:00:13 +0800

    "test the commit command"

---
+++ src/Git.ts
@@ -0,0 +1,23 @@
+// @ts-ignore: node.js module
+import { spawnSync } from 'child_process'
+import chalk from 'chalk'
+
+class Git {
+  public static addAll(): void {
+    const addAll = spawnSync('git', ['add', '--all'])
+    console.log(chalk.green('[OK] Git Add'))
+  }
+
+  public static commit(commitMessage: string = 'rua-cli'): void {
+    Git.addAll()
+    const commit = spawnSync('git', ['commit' ,'-m', `"${commitMessage}"`])
+    console.log(chalk.green(`[OK] Git Commit "${commitMessage}"`))
+  }
+  
+  public static push(commitMessage: string = 'rua-cli'): void {
+    Git.commit(commitMessage)
+    const commit = spawnSync('git', ['push'])
+  }
+}
+
+export default Git
