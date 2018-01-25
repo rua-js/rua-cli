61a656b2a66eb6ed2f9e351f05e24b5c5d6f7518
copydog <775150@qq.com>
2018-01-26 00:51:34 +0800

    add situation

--- src/Git.ts
+++ src/Git.ts
@@ -4,23 +4,25 @@ import chalk from 'chalk'
 
 class Git {
   public static addAll(): void {
-    const addAll = spawnSync('git', ['add', '--all'])
-    if (addAll.error instanceof Error) {
-      throw addAll.error
-    }
+    const cmd = spawnSync('git', ['add', '--all'])
+    if (cmd.error instanceof Error) {
+      throw cmd.error
+    } else if (cmd.status !== 0) {
+
+    } else
     console.log(chalk.green('[OK] Git Add'))
   }
 
   public static commit(commitMessage: string = 'rua-cli'): void {
     Git.addAll()
-    const commit = spawnSync('git', ['commit' ,'-m', `${commitMessage}`])
+    const commit = spawnSync('git', ['commit', '-m', `${commitMessage}`])
     if (commit.error instanceof Error) {
       throw commit.error
     }
     console.log(commit)
     console.log(chalk.green(`[OK] Git Commit "${commitMessage}"`))
   }
-  
+
   public static push(commitMessage: string = 'rua-cli'): void {
     Git.commit(commitMessage)
     const push = spawnSync('git', ['push'])
